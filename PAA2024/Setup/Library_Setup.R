13c9f4d66414a677920c3c7fb47da42d60c25c55
Chirag Patel <pfard@uwaterloo.ca>
2024-04-11 18:51:45 +0000

    Created the workshop folder structure

---
+++ PAA2024/Setup/Library_Setup.R
@@ -0,0 +1,7 @@
+install.packages("sf") # main package to work with spatial vector data in R
+install.packages("tidyverse") # suit of packages to handle and visualize data
+install.packages("mapview") # provides easy way to create interactive maps
+install.packages("tigris") # provides a large range of geographical data for the US(e.g:census tracts, counties, roads)
+install.packages("terra") # spatial data analysis with vector and raster data
+install.packages("crsuggest") # allows to find the most suitable CRS for a certain location
+install.packages("tidycensus") #download census and ACS data
