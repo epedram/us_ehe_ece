ae239c629c02c23d3842e2d92fc7555283a805bf
Pedram Fard <epedram@users.noreply.github.com>
2024-10-04 19:48:50 -0400

    Update 0501_ece_ehe_summaries using minmax

--- iii_extreme_events_identification/0501_ece_ehe_summaries.R
+++ iii_extreme_events_identification/0501_ece_ehe_summaries.R
@@ -1,23 +1,31 @@
 library(tictoc)
+
 params_suffix <- paste0(threshold_label, "_", distribution_set)
 
-glimpse(EHE_ECE_compiled)
+plots_output_path <- file.path(runtime_path, paste0("plots_summary_stats_", EHCE_Param))
+if (!dir.exists(plots_output_path)) {
+  dir.create(file.path(runtime_path, paste0("plots_summary_stats_", EHCE_Param)))
+} 
 
-col_list <- c("apparent_temperature", 
-              "temperature_avg",
-              "wind_speed_avg",
-              "temperature_dewpoint_avg",
-              "relative_humidity_avg",
-              "vapure_pressure_avg")
+col_list <- c(  "d_temperature_avg",
+                "d_temperature_min",
+                "d_temperature_max",
+                
+                "d_relative_humidity_avg",
+                "d_relative_humidity_min",
+                "d_relative_humidity_max",
+                
+                "d_wind_speed_avg",
+                "d_wind_speed_min",
+                "d_wind_speed_max")
 
-vfx_histocomparison(stations_daily_means_compiled,
-                    EHE_ECE_compiled,
+vfx_histocomparison(S973_Sheridan_compiled_with_stations_daily_means,
+                    S989_T002_EHE_ECE_events_only,
                     col_list,
                     params_output_path,
                     params_suffix)
 
-ehe_ece <- EHE_ECE_events_only
-ls(ehe_ece)
+ehe_ece <- S989_T002_EHE_ECE_events_only
 
 group_vars_universal = c("completeness_code", "EF_distribution_set")
 group_vars_annual = c("year_number", "completeness_code", "EF_distribution_set")
@@ -26,27 +34,63 @@ group_vars_annual_by_type = c("year_number", "completeness_code","EF_distributio
 
 group_vars_monthly_by_type = c("month_name", "month_number", "completeness_code", "EF_distribution_set", "Event_Type")
 
-summary_vars = c("Event_duration", "DM_AT", "EHF", "ECF",
-                 "relative_humidity_avg", "temperature_avg", "wind_speed_avg")
+summary_vars = c("Event_duration", 
+                 "EHF", "ECF",
+                 
+                 "d_apparent_temperature_avg",
+                 "d_apparent_temperature_min",
+                 "d_apparent_temperature_max",
+                 
+                 "d_heat_index_heatstress_lib_avg",
+                 "d_heat_index_heatstress_lib_min",
+                 "d_heat_index_heatstress_lib_max",
+                 
+                 "d_wbgt_heatstress_lib_avg",
+                 "d_wbgt_heatstress_lib_min",
+                 "d_wbgt_heatstress_lib_max",
+                 
+                 "d_temperature_avg",
+                 "d_temperature_min",
+                 "d_temperature_max",
+                 
+                 "d_relative_humidity_avg",
+                 "d_relative_humidity_min",
+                 "d_relative_humidity_max",
+                 
+                 "d_vapoure_pressure_avg",
+                 "d_vapoure_pressure_min",
+                 "d_vapoure_pressure_max",
+                 
+                 "d_wind_speed_avg",
+                 "d_wind_speed_min",
+                 "d_wind_speed_max")
+
+ptables_output_path <- file.path(reports_path, paste0("events_summary_stats"))
+if (!dir.exists(ptables_output_path)) {
+  dir.create(file.path(reports_path, paste0("events_summary_stats")))
+} 
+
+meta_ptables_output_path <- file.path(ptables_output_path, paste0("events_summary_stats_", EHCE_Param))
+if (!dir.exists(meta_ptables_output_path)) {
+  dir.create(file.path(ptables_output_path, paste0("events_summary_stats", EHCE_Param)))
+} 
 
 tic()
-ehe_ece_u <- events_summary_stats(ehe_ece, group_vars_universal, summary_vars, params_suffix, 1)
-ehe_ece_ut <- events_summary_stats(ehe_ece, group_vars_universal_by_type, summary_vars, params_suffix, 1)
-ehe_ece_a <- events_summary_stats(ehe_ece, group_vars_annual, summary_vars, params_suffix, 1)
-ehe_ece_at <- events_summary_stats(ehe_ece, group_vars_annual_by_type, summary_vars, params_suffix, 1)
-ehe_ece_mt <- events_summary_stats(ehe_ece, group_vars_monthly_by_type, summary_vars, params_suffix, 1)
-toc()
+ehe_ece_u <- events_summary_stats(ehe_ece, group_vars_universal, summary_vars, 
+                                  params_suffix, 1, ptables_output_path)
 
-#ehe_ece_e <- events_summary_stats(ehe_ece, group_vars_event, summary_vars, 1)
-fx_reportFieldsPar(
-  ehe_ece_a,
-  ehe_ece_u,
-  ehe_ece_ut,
-  ehe_ece_at,
-  ehe_ece_mt,
-  
-  suffix = st_label,
-  output_path = stats_outputs_path)
+ehe_ece_ut <- events_summary_stats(ehe_ece, group_vars_universal_by_type, summary_vars,  
+                                   params_suffix, 1, ptables_output_path)
+
+ehe_ece_a <- events_summary_stats(ehe_ece, group_vars_annual, summary_vars,  
+                                  params_suffix, 1, ptables_output_path)
+
+ehe_ece_at <- events_summary_stats(ehe_ece, group_vars_annual_by_type, summary_vars,  
+                                   params_suffix, 1, ptables_output_path)
+
+ehe_ece_mt <- events_summary_stats(ehe_ece, group_vars_monthly_by_type, summary_vars,  
+                                   params_suffix, 1, ptables_output_path)
+toc()
 
 group_vars_event = c("EUID", "completeness_code", "EF_distribution_set", "Event_Type")
 
@@ -54,22 +98,33 @@ col_dict <- list(
   c("Event_duration_avg", "Average Event Duration", "Event_duration_sd"),
   c("Event_duration_max", "Max Event Duration", "Event_duration_sd"),
   
+  c("d_temperature_avg_median", "Median Average Temperature", "d_temperature_avg_sd"),
+  c("d_temperature_min_median", "Median Min Temperature", "d_temperature_min_sd"),
+  c("d_temperature_max_median", "Median Max Temperature", "d_temperature_max_sd"),
+  
+  c("d_wind_speed_avg_median", "Median Average Wind-speed", "d_wind_speed_avg_sd"),
+  c("d_relative_humidity_avg_median", "Median Average Relative", "d_relative_humidity_avg_sd"),
+  c("d_vapoure_pressure_avg_median", "Median Average Vapour Pressure", "d_vapoure_pressure_avg_sd"),
+  
+  c("d_apparent_temperature_avg_median", "Median Average AT", "d_apparent_temperature_avg_sd"),
+  c("d_apparent_temperature_min_median", "Median Min AT", "d_apparent_temperature_min_sd"),
+  c("d_apparent_temperature_max_median", "Median Max AT", "d_apparent_temperature_max_sd"),
+  
+  c("d_heat_index_heatstress_lib_avg_median", "Median Average HI", "d_heat_index_heatstress_lib_avg_sd"),
+  c("d_heat_index_heatstress_lib_min_median", "Median Min HI", "d_heat_index_heatstress_lib_min_sd"),
+  c("d_heat_index_heatstress_lib_max_median", "Median Max HI", "d_heat_index_heatstress_lib_max_sd"),
+  
+  c("d_wbgt_heatstress_lib_avg_median", "Median Average WBGT", "d_wbgt_heatstress_lib_avg_sd"),
+  c("d_wbgt_heatstress_lib_min_median", "Median Min WBGT", "d_wbgt_heatstress_lib_min_sd"),
+  c("d_wbgt_heatstress_lib_max_median", "Median Max WBGT", "d_wbgt_heatstress_lib_max_sd"),
+  
   c("Distinct_Impacted_Days", "Impacted Days", "Event_duration_sd"),
   c("Distinct_Impacted_Stations", "Impacted Stations", "Event_duration_sd"),
   c("Average_Days_per_Impacted_Station", "Average Days per Impacted Station", "Event_duration_sd"),
+  c("Distinct_Events_Universal", "Distinct Events (Universal)", "Event_duration_sd"))
   
-  c("Distinct_Events_Universal", "Distinct Events (Universal)", "Event_duration_sd"),
-  c("temperature_avg_median", "Median Average Temperature", "temperature_avg_sd"),
-  c("wind_speed_avg_median", "Median Average Wind-speed", "wind_speed_avg_sd"),
-  c("relative_humidity_avg_median", "Median Average Relative", "relative_humidity_avg_sd"),
-  
-  c("DM_AT_median", "Median AT", "DM_AT_sd"))
-
-
 summary_vars_y_u = c("Event_duration_max", "Event_duration_avg",
                      "Distinct_Events_Universal", "Distinct_Impacted_Stations")
-
-
 # Plots -----
 map(col_dict, ~fx_lineplot_errbar(ehe_ece_at,
                                   params_output_path,
@@ -87,6 +142,7 @@ map(col_dict, ~fx_lineplot_errbar(ehe_ece_mt,
                                   .x[3],
                                   facet_formula = "completeness_code + EF_distribution_set ~ Event_Type"))
 
+
 fx_lineplot(ehe_ece_at,
             params_output_path,
             "year_number",
@@ -102,12 +158,9 @@ fx_lineplot(ehe_ece_at,
             "Total Days Impacted by Extreme Events",
             facet_formula = "cc_label + EF_distribution_set ~ Event_Type")
 
-
 bar_col_dict <- list(
   c("Distinct_Events_Universal", "Frequency of Extreme Events (Universal)"),
-  c("Distinct_Impacted_Stations", "Total Stations Impacted by Extreme Events")
-  #c("Distinct_Events_Stations", "Frequency of Extreme Events factored by Impacted Stations")
-)
+  c("Distinct_Impacted_Stations", "Total Stations Impacted by Extreme Events"))
 
 map(bar_col_dict, ~fx_events_barplot(ehe_ece_at,
                                      params_output_path,
@@ -124,13 +177,3 @@ map(bar_col_dict, ~fx_events_barplot(ehe_ece_mt,
                                      .x[1],
                                      .x[2],
                                      facet_formula = "EF_distribution_set ~ Event_Type"))
-
-ls(EHE_ECE_events_only)
-
-fx_histboxplot(EHE_ECE_events_only, "month_name", 
-                  params_output_path, params_suffix) # // . ----[histoboxplot]---- . ----
-fx_histboxplot(EHE_ECE_events_only, "year_number", 
-                  params_output_path, params_suffix)
-
-rm(EHE_ECE_events_only)
-
